d8f50a3a25e2b5341b4ef7adf13ec7a59b2b5b03
chestahab <chestahab@gmail.com>
2017-04-16 20:36:38 -0400

    Add files via upload

---
+++ Instagram Api Request/sketch.js
@@ -0,0 +1,31 @@
+var likes
+
+var api = 'https://api.instagram.com/v1/media/';
+var apiKey = '?access_token=40290595.1677ed0.8e890906a6054d7eaf29ebe57bf68e05';
+
+var input;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  var button = select('#submit');
+  button.mousePressed(likeAsk);
+  
+  input = select('#mediaid')
+}  
+  
+function likeAsk() {
+  var url = api + input.value() + apiKey
+  loadJSON(url, gotData, 'jsonp')
+
+}
+function gotData(data) {
+  print(data)
+  likes = data;
+}
+
+function draw() {
+  background(0);
+
+
+}
